efb0fb16623635cebe7280718f7d85b6bbe30a60
Vishwanathms <vishwanath.murthy@gmail.com>
2024-08-28 11:19:35 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  alias = "dev"
+  region = var.location
+  #profile = "aws-b1-d1"
+  #profile = "specnet-tf-aug-vms"
+}
+
+
+resource "aws_vpc" "main_dev" {
+  provider = aws.dev
+  cidr_block = var.cidr
+  tags = {
+    Name = var.tag_name
+    owner = "vish"
+    dep = "hr"
+    env = var.env
+  }
+}
